26dbd901b032c9d7edd7acf95f982652cc839860
George Jones <georgekeesjones@gmail.com>
2025-07-09 11:14:35 +0300

    create grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,17 @@
 function init(){
+    // All code goes here
+    const gridElem = document.querySelector('.grid')
+
+    function createGrid(){
+        // for every cell that we require create a div
+        // append this cell to our grid
+        for (let i = 0; i < 100; i++){
+            const cell = document.createElement('div')
+            cell.textContent = i
+            gridElem.appendChild(cell)
+        }
+    }
+    createGrid()
 
 }
 
