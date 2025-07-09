f0f71224e158b9efc7dbfe1fdbf5a0a9cdbda4fa
George Jones <georgekeesjones@gmail.com>
2025-07-09 14:37:08 +0300

    finished game

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
-function init(){
+function init() {
     // All code goes here
     const gridElem = document.querySelector('.grid')
     const scoreElem = document.querySelector('#score-display')
+    const audioElem = document.querySelector('#quack')
 
     const cells = []
     const gridWidth = 10
@@ -9,36 +10,53 @@ function init(){
 
     let duckPosition = 3
     let score = 0
+    let totalDucks = 0
 
-    function addDuck(){
+    function addDuck() {
         cells[duckPosition].classList.add('duck')
+        totalDucks++
     }
 
-    function removeDuck(){
+    function removeDuck() {
         cells[duckPosition].classList.remove('duck')
     }
 
-    function play(){
-        setInterval(() => {
-            removeDuck()
-            duckPosition = Math.floor(Math.random() * numberOfCells)
-            addDuck()
-        }, 3000)
+    function endGame(){
+        alert('Game ended, score: ' + score)
+        score = 0
+        scoreElem.textContent = 'Your score is 0'
+        totalDucks = 0
     }
-   
-    function handleClick(event){
+
+    function play() {
+        setInterval(() => {
+            if (totalDucks < 10) {
+                removeDuck()
+                duckPosition = Math.floor(Math.random() * numberOfCells)
+                addDuck()
+            } else {
+                endGame()
+            }
+
+        }, 1000)
+    }
+
+    function handleClick(event) {
         console.log('Handle Click has run!')
-        if (event.target.classList.contains('duck')){
+        if (event.target.classList.contains('duck')) {
+            audioElem.pause()
+            audioElem.currentTime = 0
             score += 10
             scoreElem.textContent = `Your score is ${score}`
+            audioElem.play()
             console.log(score)
         }
     }
-    
-    function createGrid(){
+
+    function createGrid() {
         // for every cell that we require create a div
         // append this cell to our grid
-        for (let i = 0; i < numberOfCells; i++){
+        for (let i = 0; i < numberOfCells; i++) {
             const cell = document.createElement('div')
             // cell.classList.add('duck')
             cell.textContent = i
